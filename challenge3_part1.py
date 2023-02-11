64649a48cfc1383b52089aaf41117b53a836e421
Justin Purcell <123785630+jjpmesm@users.noreply.github.com>
2023-02-11 13:17:20 -0500

    Add files via upload

---
+++ challenge3_part1.py
@@ -0,0 +1,40 @@
+# Replicate this tree of directories and subdirectories:
+
+# ├── draft_code
+# |   ├── pending
+# |   └── complete
+# ├── includes
+# ├── layouts
+# |   ├── default
+# |   └── post
+# |       └── posted
+# └── site
+# Using os.system or os.mkdirs replicate this simple directory tree.
+# Delete the directory tree without deleting your entire hard drive.
+
+# import os
+#
+# os.mkdir(r"D:\draft_code")
+# os.mkdir(r"D:\draft_code\pending")
+# os.mkdir(r"D:\draft_code\complete")
+#
+# os.mkdir(r"D:\includes")
+#
+# os.mkdir(r"D:\layouts")
+# os.mkdir(r"D:\layouts\default")
+# os.mkdir(r"D:\layouts\post")
+# os.mkdir(r"D:\layouts\post\posted")
+#
+# os.mkdir(r"D:\site")
+#
+#
+# os.removedirs(r"D:\draft_code\pending")
+# os.removedirs(r"D:\draft_code\complete")
+#
+#
+# os.rmdir(r"D:\includes")
+#
+# os.removedirs(r"D:\layouts\post\posted")
+# os.removedirs(r"D:\layouts\default")
+#
+# os.rmdir(r"D:\site")
