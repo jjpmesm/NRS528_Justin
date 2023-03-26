9b320f9975f766e42d88f0503d29b98c80c3063e
Andy Davies <andy.marinebio@googlemail.com>
2023-03-26 17:37:41 -0400

    Added Andy feedback

--- challenge3/challenge3_part1.py
+++ challenge3/challenge3_part1.py
@@ -12,29 +12,29 @@
 # Using os.system or os.mkdirs replicate this simple directory tree.
 # Delete the directory tree without deleting your entire hard drive.
 
-# import os
-#
-# os.mkdir(r"D:\draft_code")
-# os.mkdir(r"D:\draft_code\pending")
-# os.mkdir(r"D:\draft_code\complete")
-#
-# os.mkdir(r"D:\includes")
-#
-# os.mkdir(r"D:\layouts")
-# os.mkdir(r"D:\layouts\default")
-# os.mkdir(r"D:\layouts\post")
-# os.mkdir(r"D:\layouts\post\posted")
-#
-# os.mkdir(r"D:\site")
-#
-#
-# os.removedirs(r"D:\draft_code\pending")
-# os.removedirs(r"D:\draft_code\complete")
-#
-#
-# os.rmdir(r"D:\includes")
-#
-# os.removedirs(r"D:\layouts\post\posted")
-# os.removedirs(r"D:\layouts\default")
-#
-# os.rmdir(r"D:\site")
+import os
+
+os.mkdir(r"D:\draft_code")
+os.mkdir(r"D:\draft_code\pending")
+os.mkdir(r"D:\draft_code\complete")
+
+os.mkdir(r"D:\includes")
+
+os.mkdir(r"D:\layouts")
+os.mkdir(r"D:\layouts\default")
+os.mkdir(r"D:\layouts\post")
+os.mkdir(r"D:\layouts\post\posted")
+
+os.mkdir(r"D:\site")
+
+
+os.removedirs(r"D:\draft_code\pending")
+os.removedirs(r"D:\draft_code\complete")
+
+
+os.rmdir(r"D:\includes")
+
+os.removedirs(r"D:\layouts\post\posted")
+os.removedirs(r"D:\layouts\default")
+
+os.rmdir(r"D:\site")
