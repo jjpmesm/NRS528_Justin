4795b692c9fd871af5f4918ae95392861e0caa8f
Justin Purcell <justin_purcell@uri.edu>
2023-04-06 14:32:08 -0400

    Update challenge5.py
    Revised version of coding challenge #5

--- challenge5/challenge5.py
+++ challenge5/challenge5.py
@@ -1,7 +1,7 @@
 # Challenge #5: Heat maps for the bull shark and whale shark (sourced from obis).
 
-# In this coding challenge I'll create two species distribution heat maps that are from a .csv file. The .csv file is
-# split into two separate species .csv files and both are converted into point shapefiles that represent species
+# In this coding challenge, I'll create two species distribution heat maps that are from a single .csv file. The .csv file is
+# split into two separate species files and both are converted into point shapefiles that represent species
 # locations. A fishnet is generated over these points to set up the final heat map for both species. You only need to
 # change line 13 for the coding challenge to your file location.
 
@@ -23,7 +23,7 @@ with open("species_list.csv") as species_csv:
     for row in csv.reader(species_csv):
         if row[0] not in species_list:
             species_list.append(row[0])
-    print(species_list)
+    print("The two species in species_file.csv are: " + str(species_list))
 
 # Splitting the larger .csv species file into separate .csv files based on species
 
@@ -38,10 +38,9 @@ for species in species_list:
     with open("species_list.csv") as longitude_csv:
         for row in csv.reader(longitude_csv):
             if row[0] == species:
-                file.write(header)
                 s_count = 0
-            file.write(",".join(row))
-            file.write("\n")
+                file.write(",".join(row))
+                file.write("\n")
 
     file.close()
 
@@ -58,12 +57,12 @@ for species in species_list:
     lyr = arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, spRef, "")
 
 # Print the total rows and create the file for the shape files
-    print("The total rows in " + species + ".csv are: " + str((arcpy.GetCount_management(out_Layer))))
+    print("The total rows in " + species + ".csv: " + str((arcpy.GetCount_management(out_Layer))))
 
     arcpy.CopyFeatures_management(lyr, saved_Layer)
 
     if arcpy.Exists(saved_Layer):
-        print("Created " + species + " shapefile successfully!")
+        print("Created " + species + " point shapefile successfully!")
 
 
 # describing the extent of species point shapefiles
@@ -79,11 +78,11 @@ for species in species_list:
 # Creating the fishnet from the species point shapefiles
     outFeatureClass = r"Species_Directory/" + species + "_Fishnet.shp"
 
-    # Set the origin of the fishnet
+# Setting the origin of the fishnet
     originCoordinate = str(XMin) + " " + str(YMin)
     yAxisCoordinate = str(XMin) + " " + str(YMin + 1)
-    cellSizeWidth = "0.25"
-    cellSizeHeight = "0.25"
+    cellSizeWidth = "2"
+    cellSizeHeight = "2"
     numRows = ""
     numColumns = ""
     oppositeCorner = str(XMax) + " " + str(YMax)
@@ -96,13 +95,13 @@ for species in species_list:
                                 oppositeCorner, labels, templateExtent, geometryType)
 
     if arcpy.Exists(outFeatureClass):
-        print("Created " + species + " Fishnet file successfully!")
+        print("Created " + species + " fishnet file successfully!")
 
 # creating the heatmap from the species fishnet
 
-    target_features=r"Species_Directory/" + species + "_Fishnet.shp"
-    join_features=r"Species_Directory/" + species + "_Output.shp"
-    out_feature_class=r"Species_Directory/" + species + "_HeatMap.shp"
+    target_features=r"Species_Directory/" +species + "_Fishnet.shp"
+    join_features=r"Species_Directory/" +species + "_Output.shp"
+    out_feature_class=r"Species_Directory/" +species + "_HeatMap.shp"
     join_operation="JOIN_ONE_TO_ONE"
     join_type="KEEP_ALL"
     field_mapping=""
@@ -114,79 +113,9 @@ for species in species_list:
                                join_operation, join_type, field_mapping, match_option,
                                search_radius, distance_field_name)
 
+# checking for file existence and deleting excess files
     if arcpy.Exists(out_feature_class):
-        print("Created " + species + "Heatmap file successfully!")
+        print("Created " + species + " heatmap file successfully!")
         arcpy.Delete_management(target_features)
         arcpy.Delete_management(join_features)
-
-#
-# # The next step is to create a shape file for the Rhincodon.csv
-#
-# in_Table = r"Rhincodon.csv"
-# x_coords = "longitude"
-# y_coords = "latitude"
-# out_Layer = "Rhincodon"
-# saved_Layer = r"Rhincodon_output.shp"
-#
-# # Set the spatial reference
-# spRef = arcpy.SpatialReference(4326)
-# lyr = arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, spRef, "")
-#
-# # Print the total rows
-# print("The total rows in Rhincodon.csv are: " + str((arcpy.GetCount_management(out_Layer))))
-#
-# arcpy.CopyFeatures_management(lyr, saved_Layer)
-# if arcpy.Exists(saved_Layer):
-#     print("Created Rhincodon shapefile successfully!")
-#
-# desc = arcpy.Describe("Rhincodon_output.shp")
-# XMin = desc.extent.XMin
-# XMax = desc.extent.XMax
-# YMin = desc.extent.YMin
-# YMax = desc.extent.YMax
-#
-# # designating the spatial reference for Rhincodon_output.shp
-# arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(4326)
-#
-# # Creating the Rhincodon_output.shp
-# outFeatureClass = "Rhincodon_Fishnet.shp"
-#
-# # Set the origin of the fishnet
-# originCoordinate = str(XMin) + " " + str(YMin)
-# yAxisCoordinate = str(XMin) + " " + str(YMin + 1)
-# cellSizeWidth = "5"
-# cellSizeHeight = "5"
-# numRows = ""
-# numColumns = ""
-# oppositeCorner = str(XMax) + " " + str(YMax)
-# labels = "NO_LABELS"
-# templateExtent = "#"
-# geometryType = "POLYGON"
-#
-# arcpy.CreateFishnet_management(outFeatureClass, originCoordinate, yAxisCoordinate,
-#                                cellSizeWidth, cellSizeHeight, numRows, numColumns,
-#                                oppositeCorner, labels, templateExtent, geometryType)
-#
-# if arcpy.Exists(outFeatureClass):
-#     print("Created Rhincodon Fishnet file successfully!")
-#
-# # creating the heatmap for Rhincodon_HeatMap.shp
-#
-# target_features="Rhincodon_Fishnet.shp"
-# join_features="Rhincodon_output.shp"
-# out_feature_class="Rhincodon_HeatMap.shp"
-# join_operation="JOIN_ONE_TO_ONE"
-# join_type="KEEP_ALL"
-# field_mapping=""
-# match_option="INTERSECT"
-# search_radius=""
-# distance_field_name=""
-#
-# arcpy.SpatialJoin_analysis(target_features, join_features, out_feature_class,
-#                            join_operation, join_type, field_mapping, match_option,
-#                            search_radius, distance_field_name)
-#
-# if arcpy.Exists(out_feature_class):
-#     print("Created Rhincodon Heatmap file successfully!")
-#     arcpy.Delete_management(target_features)
-#     arcpy.Delete_management(join_features)
+        print("Excess " + str(species) + " files deleted...")
