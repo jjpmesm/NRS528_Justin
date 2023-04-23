971e6b462dacddab8f89cb11e65c2ff52224bb10
Justin Purcell <justin_purcell@uri.edu>
2023-04-23 15:51:12 -0400

    Update challenge 2 part 1

--- challenge2/challenge2_part1.py
+++ challenge2/challenge2_part1.py
@@ -5,7 +5,6 @@ list = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
 # file, one for item 1 and one for item 2. Item 2 is tricky so if you get stuck try your best (no penalty),
 # for a hint check out the solution by desiato here.
 # Make a new list that has all the elements less than 5 from this list in it and print out this new list.
-
 new_list = []
 for item in list:
     if item <5:
